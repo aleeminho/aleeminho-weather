850a1353628e99051be9f41c30d2b6ec37e228fb
aleeminho <a.fikri2411@gmail.com>
2022-07-16 14:51:59 +0700

    edit name place

--- public/src/script.js
+++ public/src/script.js
@@ -1,6 +1,5 @@
 $(() => {
   const jsonData = {};
-
   const fetchData = async (city) => {
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e0156a8e43019007a4f932d9723ea1f1&units=metric`);
@@ -12,6 +11,8 @@ $(() => {
   };
 
   function appendToHtml() {
+    $('.data p').remove();
+    $('.data').append(`<p><span>Name</span> : ${jsonData.Name}</>`);
     $('.data').append(`<p><span>Coordinate</span> : ${jsonData.Coordinate}</>`);
     $('.data').append(`<p><span>Weather</span> : ${jsonData.Weather}</>`);
     $('.data').append(`<p><span>Temperature</span> : ${jsonData.Temperature}</>`);
@@ -22,13 +23,14 @@ $(() => {
   }
 
   function inputTheData(data) {
-    jsonData.Coordinate = `Longitude: ${data.coord.lon}, Latitudde ${data.coord.lat}`;
+    jsonData.Coordinate = `Longitude: ${data.coord.lon}, Latitude ${data.coord.lat}`;
     jsonData.Weather = `${data.weather[0].main}, ${data.weather[0].description}`;
     jsonData.Temperature = `${data.main.temp}° C, ${data.main.temp_min}° C (Min temp), ${data.main.temp_max}° C (Max temp)`;
     jsonData.Humidity = `${data.main.humidity} %`;
     jsonData.Sunrise = `${new Date(data.sys.sunrise * 1000).toLocaleString()}`;
     jsonData.Sunset = `${new Date(data.sys.sunset * 1000).toLocaleString()}`;
     jsonData.Visibility = `${data.visibility} Meters`;
+    jsonData.Name = `${data.name}`;
     appendToHtml();
   }
 
